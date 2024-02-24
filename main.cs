1a7e56574c0cdb0e1e82b50fea8b327fc390a8d0
Yakitori31 <158657248+Yakitori31@users.noreply.github.com>
2024-02-24 03:28:40 +0000

    Final

--- main.cs
+++ main.cs
@@ -1,7 +1,33 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    static void Main()
+    {
+        int sum = 0;
+        string message = "";
+
+        while (true)
+        {
+            Console.Write("Enter something: ");
+            string userInput = Console.ReadLine();
+
+            if (userInput.ToLower() == "exit")
+            {
+                Console.WriteLine("Closing Program..");
+                break;
+            }
+
+            if (int.TryParse(userInput, out int number))
+            {
+                sum += number;
+                Console.WriteLine($"Adding {number} to {sum} = {sum}");
+            }
+            else
+            {
+                message += userInput;
+                Console.WriteLine($"Current Message is: {message}");
+            }
+        }
+    }
 }
